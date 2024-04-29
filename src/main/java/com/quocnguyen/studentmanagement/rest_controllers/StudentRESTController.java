728186ad4160b439500f70ea4cadbb59921a2f7e
21120317 <quocng777@gmail.com>
2024-04-29 17:20:43 +0700

    add 'GET' method for students

--- src/main/java/com/quocnguyen/studentmanagement/rest_controllers/StudentRESTController.java
+++ src/main/java/com/quocnguyen/studentmanagement/rest_controllers/StudentRESTController.java
@@ -1,7 +1,30 @@
 package com.quocnguyen.studentmanagement.rest_controllers;
 
+import com.quocnguyen.studentmanagement.entities.Student;
+import com.quocnguyen.studentmanagement.services.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class StudentRESTController {
+    private final StudentService service;
+    @GetMapping("/api/students")
+    public ResponseEntity<List<Student>> getStudents(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDir) {
+
+
+
+
+        List<Student> students = service.getStudents(page, sortBy, sortDir);
+
+        return ResponseEntity.ok(students);
+    }
 }
--- src/main/java/com/quocnguyen/studentmanagement/services/StudentService.java
+++ src/main/java/com/quocnguyen/studentmanagement/services/StudentService.java
@@ -1,13 +1,28 @@
 package com.quocnguyen.studentmanagement.services;
 
+import com.quocnguyen.studentmanagement.entities.Student;
 import com.quocnguyen.studentmanagement.repositories.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class StudentService {
     private final StudentRepository repository;
+    private static final int NUMBER_STUDENT_PER_PAGE = 12;
 
+    public List<Student> getStudents(int page, String sortBy, String sortDir) {
+
+        Sort sort = sortDir.equals("desc") ? Sort.by(sortBy).descending() : Sort.by(sortBy).ascending();
+        Pageable paging = PageRequest.of(page, NUMBER_STUDENT_PER_PAGE, sort);
+
+        return repository.findAll(paging).getContent();
+    }
 
 }
